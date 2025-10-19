20fbf95b8a416b9207e234387ac9a7887a132315
Kaal-09 <sakshamgautam369@gmail.com>
2025-10-19 14:42:12 +0530

    making parser

---
+++ 02/src/monkey/parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"github.com/Kaal-09/Interpreter-In_GoLang/01/src/monkey/lexer"
+	"github.com/Kaal-09/Interpreter-In_GoLang/01/src/monkey/token"
+	"github.com/Kaal-09/Interpreter-In_GoLang/02/src/monkey/ast"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
