993ad4051454f2a47ee5323587801e7769dbc183
Kaal-09 <sakshamgautam369@gmail.com>
2025-12-07 14:11:43 +0530

    whole parser done

---
+++ 02/src/monkey/repl/repl.go
@@ -0,0 +1,42 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/Kaal-09/Interpreter-In_GoLang/01/src/monkey/lexer"
+	"github.com/Kaal-09/Interpreter-In_GoLang/02/src/monkey/parser"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
